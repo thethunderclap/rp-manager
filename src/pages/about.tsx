5538b91a46663b3d30da01b86c6a1bd3d9ae3e29
devnrj07 <niraj.mukta123@gmail.com>
2023-08-09 23:15:14 +0530

    some fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,9 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />} />
+  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    <h1 className="text-3xl font-bold">About</h1>
+  </Main>
 );
 
 export default About;
